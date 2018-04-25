bc76bc6bf1dd5238e4ae97335d6db531cdb379ff
noraibrahimi <33460814+noraibrahimi@users.noreply.github.com>
2018-04-25 21:31:04 +0200

    Testimi i funksionit nga numer decimal ne numer binar

--- Program.cs
+++ Program.cs
@@ -10,10 +10,42 @@ namespace ConsoleApp1
 {
     class Program
     {
+        StringBuilder sb = new StringBuilder();
+        public string ToBinary(double numri)
+        {
+            numri = Math.Floor(numri);
+
+            if (numri == 1)
+            {
+                sb.Insert(0,1);
+
+                return sb.ToString();
+            }
+
+            if (numri % 2 == 0)
+            {
+                sb.Insert(0, 0);
+            }
+            else if (numri % 2 == 1)
+            {
+                sb.Insert(0, 1);
+            }
+
+            numri = numri / 2;
+            
+            ToBinary(numri);
+
+            return sb.ToString(); //nuk ekzekutohet kurr
+        }
+
+ 
 
         static void Main(string[] args)
         {
+            Program prg = new Program();
+            Console.WriteLine(prg.ToBinary(8));
 
+            Console.ReadKey();
         }
     }
 }
